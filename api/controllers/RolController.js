a89eca7c8f6d5f5d771c565cd772277e6b06557b
John Alvarez <jhon1052020@hotmail.com>
2015-12-30 16:59:09 -0500

    Crea api rol

---
+++ api/controllers/RolController.js
@@ -0,0 +1,14 @@
+/**
+ * RolController
+ *
+ * @description :: Server-side logic for managing rols
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+    myMethod : function (req, res){
+        console.log("calling method myMethod");
+        res.send("calling method myMethod");
+    }
+};
+
