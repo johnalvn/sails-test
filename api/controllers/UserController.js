2346e0763f3fb1c5f8f6f6f012aac0174ba5fb81
John Alvarez <johalv@linux-nzvr.eltiempo.com.co>
2015-12-30 13:57:43 -0500

    Inicia el repositorio

---
+++ api/controllers/UserController.js
@@ -0,0 +1,16 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+  getUser : function(req, res){
+      var user = 1;
+      res.send("user");
+  }  
+};
+
+
+    
